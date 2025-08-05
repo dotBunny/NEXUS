687b6faae689ed09eb0105ef921b4c2b110d60fd
Matthew Davey <reapazor@gmail.com>
2025-08-04 22:51:00 -0400

    Stub in version getter

--- Plugins/Core/Source/NexusCoreEditor/Private/InstanceObjects/NUpdateCheckInstanceObject.cpp
+++ Plugins/Core/Source/NexusCoreEditor/Private/InstanceObjects/NUpdateCheckInstanceObject.cpp
@@ -4,9 +4,12 @@
 #include "InstanceObjects/NUpdateCheckInstanceObject.h"
 
 #include "EditorUtilityLibrary.h"
+#include "HttpModule.h"
 #include "NCoreEditorMinimal.h"
 #include "NEditorSettings.h"
 #include "NEditorUserSettings.h"
+#include "Interfaces/IHttpRequest.h"
+#include "Interfaces/IHttpResponse.h"
 
 void UNUpdateCheckInstanceObject::Create()
 {
@@ -18,10 +21,10 @@ void UNUpdateCheckInstanceObject::Create()
 	// Let's check the last time we actually looked so that were not hammering
 	const UNEditorUserSettings* UserSettings = UNEditorUserSettings::Get();
 	const FTimespan TimeDifference = FDateTime::Now() - UserSettings->UpdatesLastChecked;
-	if (TimeDifference.GetDays() <= Settings->UpdatesFrequency)
-	{
-		return;
-	}
+	// if (TimeDifference.GetDays() <= Settings->UpdatesFrequency)
+	// {
+	// 	return;
+	// }
 
 	NE_LOG(Warning, TEXT("TIME diff %i HIT"), TimeDifference.GetDays())
 
@@ -39,15 +42,49 @@ void UNUpdateCheckInstanceObject::Execute()
 {
 	RemoveFromRoot();
 	
-	NE_LOG(Warning, TEXT("EXECUTE UPDATE"))
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetURL(GetURI());
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetTimeout(5.0f);
+    
+	HttpRequest->OnProcessRequestComplete().BindLambda([]([[maybe_unused]] FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+	{
+		if (bWasSuccessful && Response.IsValid())
+		{
+			TArray<FString> Lines;
+			Response->GetContentAsString().ParseIntoArrayLines(Lines, true);
 
+			// We go reverse because it is near the end
+			for (int i = Lines.Num() - 1; i >= 0; i--)
+			{
+				if (Lines[i].StartsWith(TEXT("#define N_VERSION_NUMBER")))
+				{
+					const FString VersionNumber = Lines[i].Replace(TEXT("#define N_VERSION_NUMBER"), TEXT("")).TrimStartAndEnd();
+					NE_LOG(Log, TEXT("[UNUpdateCheckInstanceObject::Execute] Found remote version: %s"), *VersionNumber);
+
+					if (VersionNumber.IsNumeric())
+					{
+						const int32 VersionNumberActual = FCString::Atoi(*VersionNumber);
+						const UNEditorSettings* Settings = UNEditorSettings::Get();
+					
+						if (VersionNumberActual > Settings->UpdatesIgnoreVersion)
+						{
+							// TODO: NOTICE OF UPDATE
+						}
+					}
+					
+					break;
+				}
+			}
+		}
+	});
+	HttpRequest->ProcessRequest();
+	
 	// Update last checked
 	UNEditorUserSettings* UserSettings = UNEditorUserSettings::GetMutable();
 	UserSettings->UpdatesLastChecked = FDateTime::Now();
 	UserSettings->SaveConfig();
-	
 }
-
 
 FString UNUpdateCheckInstanceObject::GetURI() const
 {
--- Plugins/Core/Source/NexusCoreEditor/Public/InstanceObjects/NUpdateCheckInstanceObject.h
+++ Plugins/Core/Source/NexusCoreEditor/Public/InstanceObjects/NUpdateCheckInstanceObject.h
@@ -1,8 +1,8 @@
 ﻿// Copyright dotBunny Inc. All Rights Reserved.
 // See the LICENSE file at the repository root for more information.
 
-#pragma once
 
+#pragma once
 #include "NUpdateCheckInstanceObject.generated.h"
 
 
