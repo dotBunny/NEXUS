7b83af12a42a6b232d80d0a6a9d1f76d32638146
Matthew Davey <reapazor@gmail.com>
2025-12-05 11:37:47 -0500

    Base widget setup

---
+++ Plugins/UI/Source/NexusUI/Public/Components/NListView.h
@@ -0,0 +1,55 @@
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+#pragma once
+
+#include "CommonListView.h"
+#include "INListViewEntry.h"
+#include "NListView.generated.h"
+
+UCLASS(BlueprintType, Blueprintable)
+/**
+ * A wrapper around the UComboListView class to allow for calls to an interface on the entry elements. 
+ **/
+class NEXUSUI_API UNListView : public UCommonListView
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFUNCTION(BlueprintCallable)
+	void SetReferenceObject(UObject* Object) { ReferenceObject = Object; }
+
+	UFUNCTION(BlueprintCallable)
+	UObject* GetReferenceObject() const
+	{
+		if (ReferenceObject.IsValid())
+		{
+			return ReferenceObject.Get();
+		}
+		return nullptr;
+	}
+
+protected:
+	virtual UUserWidget& OnGenerateEntryWidgetInternal(UObject* Item, TSubclassOf<UUserWidget> DesiredEntryClass, const TSharedRef<STableViewBase>& OwnerTable) override
+	{
+		UUserWidget& Widget =  Super::OnGenerateEntryWidgetInternal(Item, DesiredEntryClass, OwnerTable);
+		
+		if (Widget.Implements<UNListViewEntry>())
+		{
+			if (INListViewEntry* Entry = Cast<INListViewEntry>(&Widget); 
+				Entry != nullptr)
+			{
+				Entry->SetOwnerListView(&Widget, this);
+			}
+			else
+			{
+				INListViewEntry::Execute_OnSetOwnerListView(&Widget, this);
+			}
+		}
+		return Widget;
+	};
+
+	UPROPERTY(BlueprintreadOnly, Category = "NListView")
+	TWeakObjectPtr<UObject> ReferenceObject = nullptr;
+};
---
+++ Plugins/UI/Source/NexusUI/Public/Widgets/NTextListViewEntry.h
@@ -0,0 +1,30 @@
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+#pragma once
+
+#include "INListViewEntry.h"
+#include "Blueprint/UserWidget.h"
+#include "NTextListViewEntry.generated.h"
+
+class UCommonTextBlock;
+class UCommonBorder;
+
+UCLASS(BlueprintType, Blueprintable, Experimental)
+class NEXUSUI_API UNTextListViewEntry : public UUserWidget, public INListViewEntry
+{
+	GENERATED_BODY()
+
+	virtual void SetOwnerListView(UObject* Widget, UNListView* Owner) override
+	{
+		OwnerListView = Owner;
+		Execute_OnSetOwnerListView(Widget, Owner);
+	}
+	
+protected:	
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UNListView> OwnerListView;
+	
+	UPROPERTY(BlueprintReadOnly,meta=(BindWidget))
+	TObjectPtr<UCommonTextBlock> Text;
+};
