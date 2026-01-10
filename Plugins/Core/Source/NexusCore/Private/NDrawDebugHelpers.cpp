feb1a4673dbb598627c241699677f7c3f3441ef6
Matthew Davey <reapazor@gmail.com>
2026-01-10 17:12:23 -0500

    Apply some simplification to the logic for drawing strings

--- Plugins/Core/Source/NexusCore/Private/NDrawDebugHelpers.cpp
+++ Plugins/Core/Source/NexusCore/Private/NDrawDebugHelpers.cpp
@@ -11,110 +11,116 @@ void FNDrawDebugHelpers::DrawDebugString(const UWorld* InWorld, FString& String,
 	const float Scale, const float LineHeight, const float Thickness, const bool bInvertLineFeed, const bool bDrawBelowPosition)
 {
 #if ENABLE_DRAW_DEBUG
-	if (GEngine->GetNetMode(InWorld) != NM_DedicatedServer)
+	// Ensure we are not a dedicated server who doesn't have a visual side
+	if (GEngine->GetNetMode(InWorld) == NM_DedicatedServer)
 	{
-		// Find the appropriate batching component
-		ULineBatchComponent* LineBatcher = nullptr;
-		if (InWorld)
+		return;
+	}
+
+	// Find the appropriate batching component
+	ULineBatchComponent* LineBatcher = nullptr;
+	if (InWorld)
+	{
+		if (bPersistentLines || LifeTime > 0.f)
 		{
-			if (bPersistentLines || LifeTime > 0.f)
-			{
-				LineBatcher = DepthPriority ? InWorld->GetLineBatcher(UWorld::ELineBatcherType::ForegroundPersistent) : InWorld->GetLineBatcher(UWorld::ELineBatcherType::WorldPersistent);
-			}
-			else
-			{
-				LineBatcher =  DepthPriority ? InWorld->GetLineBatcher(UWorld::ELineBatcherType::Foreground) : InWorld->GetLineBatcher(UWorld::ELineBatcherType::World);
-			}
+			LineBatcher = DepthPriority ? InWorld->GetLineBatcher(UWorld::ELineBatcherType::ForegroundPersistent) : InWorld->GetLineBatcher(UWorld::ELineBatcherType::WorldPersistent);
 		}
-		
-		if (LineBatcher != nullptr)
+		else
 		{
-			float const LineLifeTime =  bPersistentLines ? -1.0f : ((LifeTime > 0.f) ? LifeTime : LineBatcher->DefaultLifeTime);
+			LineBatcher =  DepthPriority ? InWorld->GetLineBatcher(UWorld::ELineBatcherType::Foreground) : InWorld->GetLineBatcher(UWorld::ELineBatcherType::World);
+		}
+	}
+	
+	// If we can't find one, we shouldn't be trying to draw anything
+	if (LineBatcher == nullptr)
+	{
+		return;
+	}
 
-			// Ensure our glyphs are created
-			if (!FNPrimitiveFont::IsInitialized())
+	float const LineLifeTime =  bPersistentLines ? -1.0f : ((LifeTime > 0.f) ? LifeTime : LineBatcher->DefaultLifeTime);
+
+	// Ensure our glyphs are created
+	if (!FNPrimitiveFont::IsInitialized())
+	{
+		FNPrimitiveFont::Initialize();
+	}
+
+	// Calculate the working scale to multiply our glyph points by
+	const float WorkingScale = Scale * 12; // Our pseudo font-size
+
+	// Reserve some room, assuming an average of 5 lines per character
+	TArray<TCHAR>& Characters = String.GetCharArray();
+
+	float WorkingLineHeight = ((8 + LineHeight) * WorkingScale) * -1;
+	FVector BasePosition = Position;
+	if (bDrawBelowPosition)
+	{
+		BasePosition += FVector(0.f, 0.f, WorkingLineHeight);
+	}
+	FVector CurrentPosition = BasePosition;
+	int LineIndex = 0;
+	
+	// Make it stack up if inverted
+	if (bInvertLineFeed)
+	{
+		WorkingLineHeight *= -1;
+	}
+	
+	const FVector CharacterOffset = FVector(0.f, 8 * WorkingScale, 0.f);
+	const UE::Math::TRotationMatrix<double> RotationMatrix = UE::Math::TRotationMatrix(Rotation);
+	const bool bNeedsRotation = Rotation != FRotator::ZeroRotator;
+	
+	for (const auto Character : Characters)
+	{
+		switch (const int GlyphIndex = Character)
+		{
+		case 0: // String terminator, stop now
+			return;
+		case 9: // Tab (4-spaces)
+			CurrentPosition = CurrentPosition + (CharacterOffset * 4);
+			break;
+		case 10: // New Line
+		case 13:
+			LineIndex++;
+			CurrentPosition = BasePosition + FVector(0.f, 0.f, WorkingLineHeight * LineIndex);
+			break;
+		case 32: // Space
+			CurrentPosition += CharacterOffset;
+			break;
+		default:
+
+			// Get reference to glyph data
+			TArray<FNPrimitiveFontPoint>& Points = FNPrimitiveFont::GetGlyph(GlyphIndex);
+			
+			// Check our point count, if we don't have any at this point, it is considered a bad character
+			int PointCount = Points.Num();
+			if (PointCount  == 0)
 			{
-				FNPrimitiveFont::Initialize();
+				Points = FNPrimitiveFont::GetGlyph(0);
+				PointCount = FNPrimitiveFont::UndefinedPointCount;
 			}
 
-			// Calculate the working scale to multiply our glyph points by
-			const float WorkingScale = Scale * 12; // Our pseudo font-size
-
-			// Reserve some room, assuming an average of 5 lines per character
-			TArray<TCHAR>& Characters = String.GetCharArray();
-
-			float WorkingLineHeight = ((8 + LineHeight) * WorkingScale) * -1;
-			FVector BasePosition = Position;
-			if (bDrawBelowPosition)
+			// Draw our glyph
+			for (int i = 0; i < PointCount; i += 2)
 			{
-				BasePosition += FVector(0.f, 0.f, WorkingLineHeight);
-			}
-			FVector CurrentPosition = BasePosition;
-			int LineIndex = 0;
-			
-			// Make it stack up if inverted
-			if (bInvertLineFeed)
-			{
-				WorkingLineHeight *= -1;
-			}
-			
-			const FVector CharacterOffset = FVector(0.f, 8 * WorkingScale, 0.f);
-			const UE::Math::TRotationMatrix<double> RotationMatrix = UE::Math::TRotationMatrix(Rotation);
-			const bool bNeedsRotation = Rotation != FRotator::ZeroRotator;
-			
-			for (const auto Character : Characters)
-			{
-				switch (const int GlyphIndex = Character)
+				// Scale our points and bring them into 3D
+				FVector StartPoint = CurrentPosition + FVector(0.f, Points[i].X * WorkingScale, Points[i].Y * WorkingScale);
+				FVector EndPoint = CurrentPosition + FVector(0.f, Points[i + 1].X * WorkingScale, Points[i + 1].Y * WorkingScale);
+				
+				// Rotate points around base origin w/ rotation
+				if (bNeedsRotation)
 				{
-				case 0: // String terminator, stop noww
-					return;
-				case 9: // Tab (4-spaces)
-					CurrentPosition = CurrentPosition + (CharacterOffset * 4);
-					break;
-				case 10: // New Line
-				case 13:
-					LineIndex++;
-					CurrentPosition = BasePosition + FVector(0.f, 0.f, WorkingLineHeight * LineIndex);
-					break;
-				case 32: // Space
-					CurrentPosition += CharacterOffset;
-					break;
-				default:
-
-					// Get reference to glyph data
-					TArray<FNPrimitiveFontPoint>& Points = FNPrimitiveFont::GetGlyph(GlyphIndex);
-					
-					// Check our point count, if we don't have any at this point, it is considered a bad character
-					int PointCount = Points.Num();
-					if (PointCount  == 0)
-					{
-						Points = FNPrimitiveFont::GetGlyph(0);
-						PointCount = FNPrimitiveFont::UndefinedPointCount;
-					}
-
-					// Draw our glyph
-					for (int i = 0; i < PointCount; i += 2)
-					{
-						// Scale our points and bring them into 3D
-						FVector StartPoint = CurrentPosition + FVector(0.f, Points[i].X * WorkingScale, Points[i].Y * WorkingScale);
-						FVector EndPoint = CurrentPosition + FVector(0.f, Points[i + 1].X * WorkingScale, Points[i + 1].Y * WorkingScale);
-						
-						// Rotate points around base origin w/ rotation
-						if (bNeedsRotation)
-						{
-							StartPoint = BasePosition + RotationMatrix.TransformVector(StartPoint - BasePosition);
-							EndPoint = BasePosition + RotationMatrix.TransformVector(EndPoint - BasePosition);
-						}
-						
-						// Draw line
-						LineBatcher->DrawLine(StartPoint, EndPoint, ForegroundColor, DepthPriority, Thickness, LineLifeTime);
-					}
-
-					// Offset the position to get ready for the next character
-					CurrentPosition += CharacterOffset;
-					break;
+					StartPoint = BasePosition + RotationMatrix.TransformVector(StartPoint - BasePosition);
+					EndPoint = BasePosition + RotationMatrix.TransformVector(EndPoint - BasePosition);
 				}
+				
+				// Draw line
+				LineBatcher->DrawLine(StartPoint, EndPoint, ForegroundColor, DepthPriority, Thickness, LineLifeTime);
 			}
+
+			// Offset the position to get ready for the next character
+			CurrentPosition += CharacterOffset;
+			break;
 		}
 	}
 #endif	
