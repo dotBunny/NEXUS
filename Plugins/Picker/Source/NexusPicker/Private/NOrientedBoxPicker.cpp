f8a6f0b6a6bf736fff1a657edbb06508a21f3743
Matthew Davey <reapazor@gmail.com>
2026-01-31 15:12:22 -0500

    Stub out OrientedBox

---
+++ Plugins/Picker/Source/NexusPicker/Private/NOrientedBoxPicker.cpp
@@ -0,0 +1,68 @@
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+#include "NOrientedBoxPicker.h"
+#include "NavigationSystem.h"
+#include "NPickerMinimal.h"
+#include "NPickerUtils.h"
+#include "NRandom.h"
+
+#define N_PICKER_ORIENTED_BOX_PREFIX \
+	const int OutLocationsStartIndex = OutLocations.Num(); \
+	const bool bSimpleMode = Params.MinimumDimensions.IsZero(); \
+	const bool bHasRotation = !Params.Rotation.IsZero(); \
+	OutLocations.Reserve(OutLocationsStartIndex + Params.Count);
+#define N_PICKER_ORIENTED_BOX_EXTENTS \
+	const FVector MinimumExtent = 0.5f * Params.MinimumDimensions; \
+	const FVector MaximumExtent = 0.5f * Params.MaximumDimensions;
+#define N_PICKER_ORIENTED_BOX_LOCATION_SIMPLE(FloatValue) \
+	Params.Origin + FVector( \
+		FloatValue(Params.MaximumDimensions.Min.X, Params.MaximumDimensions.Max.X), \
+		FloatValue(Params.MaximumDimensions.Min.Y, Params.MaximumDimensions.Max.Y), \
+		FloatValue(Params.MaximumDimensions.Min.Z, Params.MaximumDimensions.Max.Z))
+#define N_PICKER_ORIENTED_BOX_LOCATION(FloatValue, BooleanValue) \
+	Params.Origin + FVector( \
+		BooleanValue ? FloatValue(-MinimumExtent.X, -MaximumExtent.X) : FloatValue(MinimumExtent.X, MaximumExtent.X), \
+		BooleanValue ? FloatValue(-MinimumExtent.Y, -MaximumExtent.Y) : FloatValue(MinimumExtent.Y, MaximumExtent.Y), \
+		BooleanValue ? FloatValue(-MinimumExtent.Z, -MaximumExtent.Z) : FloatValue(MinimumExtent.Z, MaximumExtent.Z))
+#define N_PICKER_ORIENTED_BOX_LOCATION_SIMPLE_ROTATION(FloatValue) \
+	Params.Origin + Params.Rotation.RotateVector(FVector( \
+		FloatValue(Params.MaximumDimensions.Min.X, Params.MaximumDimensions.Max.X), \
+		FloatValue(Params.MaximumDimensions.Min.Y, Params.MaximumDimensions.Max.Y), \
+		FloatValue(Params.MaximumDimensions.Min.Z, Params.MaximumDimensions.Max.Z)))
+#define N_PICKER_ORIENTED_BOX_LOCATION_ROTATION(FloatValue, BooleanValue) \
+	Params.Origin + Params.Rotation.RotateVector(FVector( \
+		BooleanValue ? FloatValue(-MinimumExtent.X, -MaximumExtent.X) : FloatValue(MinimumExtent.X, MaximumExtent.X), \
+		BooleanValue ? FloatValue(-MinimumExtent.Y, -MaximumExtent.Y) : FloatValue(MinimumExtent.Y, MaximumExtent.Y), \
+		BooleanValue ? FloatValue(-MinimumExtent.Z, -MaximumExtent.Z) : FloatValue(MinimumExtent.Z, MaximumExtent.Z)))
+
+#define RANDOM_FLOAT_RANGE FNRandom::Deterministic.FloatRange
+#define RANDOM_BOOL FNRandom::Deterministic.Bool()
+void FNOrientedBoxPicker::Next(TArray<FVector>& OutLocations, const FNOrientedBoxPickerParams& Params)
+{
+	N_PICKER_ORIENTED_BOX_PREFIX
+
+}
+#undef RANDOM_FLOAT_RANGE
+#undef RANDOM_BOOL
+
+#define RANDOM_FLOAT_RANGE FNRandom::NonDeterministic.FRandRange
+#define RANDOM_BOOL FNRandom::NonDeterministic.FRandRange(0.0f, 1.0f) >= 0.5f
+void FNOrientedBoxPicker::Random(TArray<FVector>& OutLocations, const FNOrientedBoxPickerParams& Params)
+{
+	N_PICKER_ORIENTED_BOX_PREFIX
+}
+#undef RANDOM_FLOAT_RANGE
+#undef RANDOM_BOOL
+
+#define RANDOM_FLOAT_RANGE RandomStream.FRandRange
+#define RANDOM_BOOL RandomStream.FRandRange(0.0f, 1.0f) >= 0.5f
+void FNOrientedBoxPicker::Tracked(TArray<FVector>& OutLocations, int32& Seed, const FNOrientedBoxPickerParams& Params)
+{
+	const FRandomStream RandomStream(Seed);
+	
+	N_PICKER_ORIENTED_BOX_PREFIX
+	Seed = RandomStream.GetCurrentSeed();
+}
+#undef RANDOM_FLOAT_RANGE
+#undef RANDOM_BOOL
--- Plugins/Picker/Source/NexusPicker/Public/NBoxPicker.h
+++ Plugins/Picker/Source/NexusPicker/Public/NBoxPicker.h
@@ -16,7 +16,7 @@ class NEXUSPICKER_API FNBoxPicker
 public:
 	
 	/**
-	 * Generates a deterministic point in relation to an FBox.
+	 * Generates a deterministic point in relation to an axis-aligned FBox.
 	 * Uses the deterministic random generator to ensure reproducible results.
 	 * @param OutLocations An array to store the generated points.
 	 * @param Params The parameters for the point generation.
@@ -24,7 +24,7 @@ public:
 	static void Next(TArray<FVector>& OutLocations, const FNBoxPickerParams& Params);
 
 	/**
-	 * Generates a random point in relation to an FBox.
+	 * Generates a random point in relation to an axis-aligned FBox.
 	 * Uses the non-deterministic random generator for true randomness.
 	 * @param OutLocations An array to store the generated points.
 	 * @param Params The parameters for the point generation.
@@ -33,7 +33,7 @@ public:
 	
 
 	/**
-	 * Generates a random point in relation to an FBox.
+	 * Generates a random point in relation to an axis-aligned FBox.
 	 * Useful for one-time random point generation with reproducible results.
 	 * @param OutLocations An array to store the generated points.
 	 * @param Seed The random seed to use.
@@ -46,7 +46,7 @@ public:
 	}
 	
 	/**
-	 * Generates a random point in relation to an FBox.
+	 * Generates a random point in relation to an axis-aligned FBox.
 	 * Updates the seed value to enable sequential random point generation.
 	 * @param OutLocations An array to store the generated points.
 	 * @param Seed The random seed to start with, and update.
@@ -55,7 +55,7 @@ public:
 	static void Tracked(TArray<FVector>& OutLocations, int32& Seed, const FNBoxPickerParams& Params);
 
 	/**
-	 * Checks if a point is inside or on the surface of the FBox.
+	 * Checks if a point is inside or on the surface of the axis-aligned FBox.
 	 * @param Origin The center point of the FBox.
 	 * @param Dimensions The dimensions of the FBox.
 	 * @param Point The point to check.
@@ -68,7 +68,7 @@ public:
 	}
 
 	/**
-	 * Checks if multiple points are inside or on the surface of the FBox.
+	 * Checks if multiple points are inside or on the surface of the axis-aligned FBox.
 	 * @param Points The array of points to check.
 	 * @param Origin The center point of the FBox.
 	 * @param MinimumDimensions The minimum dimensions of the FBox.
