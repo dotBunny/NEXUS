caa1acc4bc9adf23933c2eec8881c2a68f0a2f33
Matthew Davey <reapazor@gmail.com>
2026-02-04 16:31:22 -0500

    Stub collision query test actor

---
+++ Plugins/Core/Source/NexusCoreEditor/Private/NCollisionQueryTestActor.cpp
@@ -0,0 +1,42 @@
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+#include "NCollisionQueryTestActor.h"
+
+ANCollisionQueryTestActor::ANCollisionQueryTestActor(const FObjectInitializer& ObjectInitializer)
+{
+	bIsEditorOnlyActor = true;
+	
+	StartPointComponent= CreateDefaultSubobject<USceneComponent>(TEXT("Start Point"));
+	StartPointComponent->SetMobility(EComponentMobility::Movable);
+	StartPointComponent->bIsEditorOnly = true;
+	RootComponent = StartPointComponent;
+	
+	EndPointComponent= CreateDefaultSubobject<USceneComponent>(TEXT("End Point"));
+	EndPointComponent->SetMobility(EComponentMobility::Movable);
+	EndPointComponent->SetupAttachment(RootComponent);
+	EndPointComponent->bIsEditorOnly = true;
+}
+
+void ANCollisionQueryTestActor::Query()
+{
+	switch (QueryMethod)
+	{
+		using enum ECollisionQueryMethod;
+	case LineTrace:
+		{
+			
+		}
+		break;
+	case Sweep:
+		{
+			
+		}
+		break;
+	case Overlap:
+		{
+			
+		}
+		break;
+	}
+}
---
+++ Plugins/Core/Source/NexusCoreEditor/Public/NCollisionQueryTestActor.h
@@ -0,0 +1,66 @@
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NCollisionQueryTestActor.generated.h"
+
+class UCameraComponent;
+class USpotLightComponent;
+
+UENUM(BlueprintType)
+enum class ECollisionQueryMethod : uint8
+{
+	LineTrace,
+	Sweep,
+	Overlap
+};
+
+UENUM(BlueprintType)
+enum class ECollisionQueryType : uint8
+{
+	Single,
+	Multi,
+	Test
+};
+
+UENUM(BlueprintType)
+enum class ECollisionQueryBy : uint8
+{
+	Channel,
+	ObjectType,
+	Profile
+};
+
+
+/**
+ * Used to test collision queries in a given map.
+ */
+UCLASS(DisplayName = "NEXUS: Collision Query Test Actor", ClassGroup = "NEXUS",
+	HideCategories=(Activation, AssetUserData, Cooking, Navigation, Tags, Actor, Input, DataLayers, LevelInstance, WorldPartition, HLOD, LOD, Rendering))
+class NEXUSCOREEDITOR_API ANCollisionQueryTestActor: public AActor
+{
+	GENERATED_BODY()
+
+	explicit ANCollisionQueryTestActor(const FObjectInitializer& ObjectInitializer);
+
+public:
+	UPROPERTY(VisibleInstanceOnly)
+	TObjectPtr<USceneComponent> StartPointComponent;
+	UPROPERTY(VisibleInstanceOnly)
+	TObjectPtr<USceneComponent> EndPointComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Collision Query", DisplayName="Method")
+	ECollisionQueryMethod QueryMethod = ECollisionQueryMethod::LineTrace;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Query", DisplayName="Type")
+	ECollisionQueryType QueryType = ECollisionQueryType::Single;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Query", DisplayName="By")
+	ECollisionQueryBy QueryBy = ECollisionQueryBy::Channel;
+	
+private:
+	void Query();
+	
+};
