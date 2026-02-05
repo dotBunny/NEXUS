5ce6966d197fadc7c808158dfb236066cb30b6cd
Matthew Davey <reapazor@gmail.com>
2026-02-04 21:57:56 -0500

    Line trace v1

--- Plugins/Core/Source/NexusCoreEditor/Private/NCollisionQueryTestActor.cpp
+++ Plugins/Core/Source/NexusCoreEditor/Private/NCollisionQueryTestActor.cpp
@@ -2,6 +2,17 @@
 // See the LICENSE file at the repository root for more information.
 
 #include "NCollisionQueryTestActor.h"
+#include "NColor.h"
+
+#define N_COLLISION_QUERY_TEST_COMMON \
+	const FVector StartPosition = GetActorLocation(); \
+	const FVector EndPosition = EndPointComponent->GetComponentLocation(); \
+	bool bHit = false; \
+	const UWorld* World = GetWorld();
+#define N_COLLISION_QUERY_HIT_COLOR FNColor::GetColor(ENColor::NC_Green)
+#define N_COLLISION_QUERY_MID_COLOR FNColor::GetColor(ENColor::NC_BlueMid)
+#define N_COLLISION_QUERY_MISS_COLOR FNColor::GetColor(ENColor::NC_Red)
+
 
 ANCollisionQueryTestActor::ANCollisionQueryTestActor(const FObjectInitializer& ObjectInitializer)
 {
@@ -10,33 +21,241 @@ ANCollisionQueryTestActor::ANCollisionQueryTestActor(const FObjectInitializer& O
 	StartPointComponent= CreateDefaultSubobject<USceneComponent>(TEXT("Start Point"));
 	StartPointComponent->SetMobility(EComponentMobility::Movable);
 	StartPointComponent->bIsEditorOnly = true;
+#if WITH_EDITORONLY_DATA
+	StartPointComponent->bVisualizeComponent = true;
+#endif	
+	
 	RootComponent = StartPointComponent;
 	
 	EndPointComponent= CreateDefaultSubobject<USceneComponent>(TEXT("End Point"));
 	EndPointComponent->SetMobility(EComponentMobility::Movable);
 	EndPointComponent->SetupAttachment(RootComponent);
+	EndPointComponent->SetRelativeLocation(FVector(500.f, 0.f, 0.f));
 	EndPointComponent->bIsEditorOnly = true;
+#if WITH_EDITORONLY_DATA
+	EndPointComponent->bVisualizeComponent = true;
+#endif
+	
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
 }
 
-void ANCollisionQueryTestActor::Query()
+void ANCollisionQueryTestActor::Tick(const float DeltaSeconds)
 {
-	switch (QueryMethod)
+	TickTimer -= DeltaSeconds;
+	if (TickTimer <= 0)
 	{
-		using enum ECollisionQueryMethod;
-	case LineTrace:
-		{
-			
-		}
-		break;
-	case Sweep:
-		{
-			
-		}
-		break;
-	case Overlap:
-		{
-			
-		}
-		break;
+		Query(UpdateTimer);
+		TickTimer = UpdateTimer;
 	}
+	
+	Super::Tick(DeltaSeconds);	
+}
+
+TArray<FName> ANCollisionQueryTestActor::GetCollisionProfileNames()
+{
+	TArray<FName> Names;
+	const UCollisionProfile* CollisionProfile = UCollisionProfile::Get();
+	const int32 Count = CollisionProfile->GetNumOfProfiles();
+	Names.Reserve(Count);
+
+	for (int32 i = 0; i < Count; ++i)
+	{
+		const FCollisionResponseTemplate* ProfileTemplate = CollisionProfile->GetProfileByIndex(i);
+		Names.Add(ProfileTemplate->Name);
+	}
+
+	return MoveTemp(Names);
+}
+
+void ANCollisionQueryTestActor::Query(const float DrawTime) const
+{
+	if (QueryMethod == ECollisionQueryTestMethod::LineTrace)
+	{
+		if (QueryPrefix == ECollisionQueryTestPrefix::Single)
+		{
+			DoLineTraceSingle(DrawTime);
+		}
+		else if (QueryPrefix == ECollisionQueryTestPrefix::Multi)
+		{
+			DoLineTraceMulti(DrawTime);
+		}
+		else if (QueryPrefix == ECollisionQueryTestPrefix::Test)
+		{
+			DoLineTraceTest(DrawTime);
+		}
+	}
+	else if (QueryMethod == ECollisionQueryTestMethod::Sweep)
+	{
+	
+	}
+	else if (QueryMethod == ECollisionQueryTestMethod::Overlap)
+	{
+	
+	}
+}
+
+void ANCollisionQueryTestActor::DoLineTraceSingle(const float DrawTime) const
+{
+	N_COLLISION_QUERY_TEST_COMMON
+	FHitResult HitResult;
+	
+	if (QueryBy == ECollisionQueryTestBy::Channel)
+	{
+		bHit = World->LineTraceSingleByChannel(HitResult, StartPosition, EndPosition, 
+			Channel, GetCollisionQueryParams(), GetCollisionResponseParams());
+	}
+	else if (QueryBy == ECollisionQueryTestBy::ObjectType)
+	{
+		bHit = World->LineTraceSingleByObjectType(HitResult, StartPosition, EndPosition, 
+			FCollisionObjectQueryParams(UEngineTypes::ConvertToCollisionChannel(ObjectType)));
+	}
+	else if (QueryBy == ECollisionQueryTestBy::Profile)
+	{
+		bHit = World->LineTraceSingleByProfile(HitResult, StartPosition, EndPosition, 
+			CollisionProfileName, GetCollisionQueryParams());
+	}
+		
+#if UE_ENABLE_DEBUG_DRAWING
+	if (bHit)
+	{
+		DrawDebugLine(World, StartPosition, HitResult.Location, N_COLLISION_QUERY_HIT_COLOR, 
+			false, DrawTime, SDPG_World, DrawLineThickness);
+		DrawDebugPoint(World, HitResult.ImpactPoint, DrawPointSize, N_COLLISION_QUERY_HIT_COLOR, 
+			false, DrawTime, SDPG_World);
+	}
+	else
+	{
+		DrawDebugLine(World, StartPosition, EndPosition, N_COLLISION_QUERY_MISS_COLOR, 
+			false, DrawTime, SDPG_World, DrawLineThickness);
+	}
+#endif
+}
+
+void ANCollisionQueryTestActor::DoLineTraceMulti(const float DrawTime) const
+{
+	N_COLLISION_QUERY_TEST_COMMON
+	TArray<FHitResult> HitResults;
+	if (QueryBy == ECollisionQueryTestBy::Channel)
+	{
+		bHit = World->LineTraceMultiByChannel(HitResults, StartPosition, EndPosition, 
+			Channel, GetCollisionQueryParams(), GetCollisionResponseParams());
+	}
+	else if (QueryBy == ECollisionQueryTestBy::ObjectType)
+	{
+		bHit = World->LineTraceMultiByObjectType(HitResults, StartPosition, EndPosition, 
+			FCollisionObjectQueryParams(UEngineTypes::ConvertToCollisionChannel(ObjectType)), GetCollisionQueryParams());
+	}
+	else if (QueryBy == ECollisionQueryTestBy::Profile)
+	{
+		bHit = World->LineTraceMultiByProfile(HitResults, StartPosition, EndPosition, 
+			CollisionProfileName, GetCollisionQueryParams());
+	}
+		
+#if UE_ENABLE_DEBUG_DRAWING
+	if (bHit)
+	{
+		// Adjust the end point to being the last hit point, unless it's by ObjectType.
+		FVector TraceEndPosition = HitResults.Last().Location;
+		if (QueryBy == ECollisionQueryTestBy::ObjectType)
+		{
+			TraceEndPosition = EndPosition;
+		}
+		DrawDebugLine(World, StartPosition, TraceEndPosition, N_COLLISION_QUERY_HIT_COLOR, 
+			false, DrawTime, SDPG_World, DrawLineThickness);
+	}
+	else
+	{
+		DrawDebugLine(World, StartPosition, EndPosition, 
+			HitResults.Num() > 0 ? N_COLLISION_QUERY_MID_COLOR : N_COLLISION_QUERY_MISS_COLOR, 
+			false, DrawTime, SDPG_World, DrawLineThickness);
+	}
+	for (const FHitResult& HitResult : HitResults)
+	{
+		DrawDebugPoint(World, HitResult.ImpactPoint, DrawPointSize, 
+			HitResult.bBlockingHit ? N_COLLISION_QUERY_HIT_COLOR : N_COLLISION_QUERY_MID_COLOR, 
+			false, DrawTime, SDPG_World);
+	}
+#endif
+}
+
+void ANCollisionQueryTestActor::DoLineTraceTest(const float DrawTime) const
+{
+	N_COLLISION_QUERY_TEST_COMMON
+	if (QueryBy == ECollisionQueryTestBy::Channel)
+	{
+		bHit = World->LineTraceTestByChannel(StartPosition, EndPosition, 
+			Channel, GetCollisionQueryParams(), GetCollisionResponseParams());
+	}
+	else if (QueryBy == ECollisionQueryTestBy::ObjectType)
+	{
+		bHit = World->LineTraceTestByObjectType(StartPosition, EndPosition, 
+			FCollisionObjectQueryParams(UEngineTypes::ConvertToCollisionChannel(ObjectType)), GetCollisionQueryParams());
+	}
+	else if (QueryBy == ECollisionQueryTestBy::Profile)
+	{
+		bHit = World->LineTraceTestByProfile(StartPosition, EndPosition, CollisionProfileName, GetCollisionQueryParams());
+	}
+#if UE_ENABLE_DEBUG_DRAWING
+	DrawDebugLine(World, StartPosition, EndPosition, 
+		bHit ? N_COLLISION_QUERY_HIT_COLOR : N_COLLISION_QUERY_MISS_COLOR, 
+		false, DrawTime, SDPG_World, DrawLineThickness);
+#endif
+}
+
+void ANCollisionQueryTestActor::DoSweepSingle(const float DrawTime) const
+{
+}
+
+void ANCollisionQueryTestActor::DoSweepMulti(const float DrawTime) const
+{
+}
+
+void ANCollisionQueryTestActor::DoSweepTest(const float DrawTime) const
+{
+}
+
+void ANCollisionQueryTestActor::DoOverlapSingle(const float DrawTime) const
+{
+}
+
+void ANCollisionQueryTestActor::DoOverlapMulti(const float DrawTime) const
+{
+}
+
+void ANCollisionQueryTestActor::DoOverlapTest(const float DrawTime) const
+{
+}
+
+FCollisionQueryParams ANCollisionQueryTestActor::GetCollisionQueryParams() const
+{
+	FCollisionQueryParams Parameters;
+	Parameters.bTraceComplex = bTraceComplex;
+	Parameters.bFindInitialOverlaps = bFindInitialOverlaps;
+	Parameters.bIgnoreBlocks = bIgnoreBlocks;
+	Parameters.bIgnoreTouches = bIgnoreTouches;
+	Parameters.bSkipNarrowPhase = bSkipNarrowPhase;
+	return MoveTemp(Parameters);
+}
+
+FCollisionResponseParams ANCollisionQueryTestActor::GetCollisionResponseParams() const
+{
+	FCollisionResponseParams Parameters;
+	Parameters.CollisionResponse = CollisionResponses;
+	return MoveTemp(Parameters);
+}
+
+FCollisionShape ANCollisionQueryTestActor::GetCollisionShape() const
+{
+	switch (QueryShape)
+	{
+		using enum ECollisionQueryTestShape;
+	case Box:
+		return FCollisionShape::MakeBox(ShapeBoxHalfExtents);
+	case Capsule:
+		return FCollisionShape::MakeCapsule(ShapeCapsuleRadius, ShapeCapsuleHalfHeight);
+	case Sphere:
+		return FCollisionShape::MakeSphere(SphereRadius);
+	}
+	return FCollisionShape();
 }
--- Plugins/Core/Source/NexusCoreEditor/Public/NCollisionQueryTestActor.h
+++ Plugins/Core/Source/NexusCoreEditor/Public/NCollisionQueryTestActor.h
@@ -6,39 +6,66 @@
 #include "CoreMinimal.h"
 #include "NCollisionQueryTestActor.generated.h"
 
-class UCameraComponent;
-class USpotLightComponent;
-
-UENUM(BlueprintType)
-enum class ECollisionQueryMethod : uint8
+UENUM()
+enum class ECollisionQueryTestMethod : uint8
 {
 	LineTrace,
 	Sweep,
 	Overlap
 };
 
-UENUM(BlueprintType)
-enum class ECollisionQueryType : uint8
+UENUM()
+enum class ECollisionQueryTestPrefix : uint8
 {
 	Single,
 	Multi,
 	Test
 };
 
-UENUM(BlueprintType)
-enum class ECollisionQueryBy : uint8
+UENUM()
+enum class ECollisionQueryTestSuffix : uint8
+{
+	Blocking,
+	Any,
+	Test
+};
+
+UENUM()
+enum class ECollisionQueryTestBy : uint8
 {
 	Channel,
 	ObjectType,
 	Profile
 };
 
+UENUM()
+enum class ECollisionQueryTestMobility : uint8
+{
+	Any,
+	Static,
+	Dynamic
+};
 
+
+UENUM()
+enum class ECollisionQueryTestShape : uint8
+{
+	Box,
+	Capsule,
+	Sphere
+};
+
+// TODO: Transient - cant be currently when entering a level it gets removed
 /**
- * Used to test collision queries in a given map.
+ * Used to test collision queries at design time in the editor.
+ * @remarks The was originally started after watching George Prosser's UnrealFest 2023 talk "Collision Data in UE5". 
+ * I later found the public version of the tool and was amused at how similar they were. Credits to George and the
+ * team at Studio Gobo for the great inspiration, there is just a slightly different spin on the implementation here.
+ * @ref https://www.youtube.com/watch?v=xIQI6nXFygA
+ * @ref https://github.com/StudioGobo/UECollisionQueryTools 
  */
-UCLASS(DisplayName = "NEXUS: Collision Query Test Actor", ClassGroup = "NEXUS",
-	HideCategories=(Activation, AssetUserData, Cooking, Navigation, Tags, Actor, Input, DataLayers, LevelInstance, WorldPartition, HLOD, LOD, Rendering))
+UCLASS(DisplayName = "NEXUS: Collision Query Test Actor", ClassGroup = "NEXUS", 
+	HideCategories=(Activation, Actor, AssetUserData, Collision, Cooking, DataLayers, HLOD, Input, LevelInstance, LOD, Navigation, Networking, Physics, Rendering, Replication, Tags,  WorldPartition))
 class NEXUSCOREEDITOR_API ANCollisionQueryTestActor: public AActor
 {
 	GENERATED_BODY()
@@ -46,21 +73,130 @@ class NEXUSCOREEDITOR_API ANCollisionQueryTestActor: public AActor
 	explicit ANCollisionQueryTestActor(const FObjectInitializer& ObjectInitializer);
 
 public:
-	UPROPERTY(VisibleInstanceOnly)
+	
+	virtual void Tick(float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="Method")
+	ECollisionQueryTestMethod QueryMethod = ECollisionQueryTestMethod::LineTrace;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="Type", 
+		meta=(EditCondition="QueryMethod!=ECollisionQueryTestMethod::Overlap", EditConditionHides))
+	ECollisionQueryTestPrefix QueryPrefix = ECollisionQueryTestPrefix::Single;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="Type", 
+		meta=(EditCondition="QueryMethod!=ECollisionQueryTestMethod::Overlap", EditConditionHides))
+	ECollisionQueryTestSuffix QuerySuffix = ECollisionQueryTestSuffix::Any;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="By")
+	ECollisionQueryTestBy QueryBy = ECollisionQueryTestBy::Channel;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="Channel",
+		meta=(EditCondition="QueryBy==ECollisionQueryTestBy::Channel", EditConditionHides))
+	TEnumAsByte<ECollisionChannel> Channel = ECC_Pawn;
+		
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="Collision Responses",
+		meta=(EditCondition="QueryBy==ECollisionQueryTestBy::Channel", EditConditionHides))
+	FCollisionResponseContainer CollisionResponses;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="Object Type",
+		meta=(EditCondition="QueryBy==ECollisionQueryTestBy::ObjectType", EditConditionHides))
+	TEnumAsByte<EObjectTypeQuery> ObjectType = EObjectTypeQuery::ObjectTypeQuery1;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="Collision Profile",
+		meta=(EditCondition="QueryBy==ECollisionQueryTestBy::Profile", EditConditionHides, GetOptions="GetCollisionProfileOptions"))
+	FName CollisionProfileName = TEXT("BlockAll");
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="Shape",
+		meta=(EditCondition="QueryMethod!=ECollisionQueryTestMethod::LineTrace", EditConditionHides))
+	ECollisionQueryTestShape QueryShape = ECollisionQueryTestShape::Capsule;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="Radius", 
+	meta=(EditCondition="QueryMethod!=ECollisionQueryTestMethod::LineTrace&&QueryShape==ECollisionQueryTestShape::Capsule", EditConditionHides))
+	float ShapeCapsuleRadius = 40.f;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="Half Height",
+		meta=(EditCondition="QueryMethod!=ECollisionQueryTestMethod::LineTrace&&QueryShape==ECollisionQueryTestShape::Capsule", EditConditionHides))
+	float ShapeCapsuleHalfHeight = 80.f;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="Half Extent", 
+		meta=(EditCondition="QueryMethod!=ECollisionQueryTestMethod::LineTrace&&QueryShape==ECollisionQueryTestShape::Box", EditConditionHides))
+	FVector ShapeBoxHalfExtents = FVector(25,25,25);
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Query", DisplayName="Radius", 
+		meta=(EditCondition="QueryMethod!=ECollisionQueryTestMethod::LineTrace&&QueryShape==ECollisionQueryTestShape::Sphere", EditConditionHides))
+	float SphereRadius = 42.f;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Options", DisplayName="Trace Complex")
+	bool bTraceComplex = false;
+
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Options", DisplayName="Find Initial Overlaps")
+	bool bFindInitialOverlaps = true;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Options", DisplayName="Ignore Blocks")
+	bool bIgnoreBlocks = false;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Options", DisplayName="Ignore Touches")
+	bool bIgnoreTouches = false;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Options", DisplayName="Skip Narrow Phase")
+	bool bSkipNarrowPhase = false;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Options", DisplayName="Mobility Type")
+	ECollisionQueryTestMobility QueryMobility = ECollisionQueryTestMobility::Any;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Options", DisplayName="Update Timer", meta=(Tooltip="How long between queries."))
+	float UpdateTimer = 0.f;
+
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Drawing", DisplayName="Line Thickness")
+	float DrawLineThickness = 1.5f;
+	
+	UPROPERTY(EditAnywhere, Category = "Collision Test|Drawing", DisplayName="Point Size")
+	float DrawPointSize = 15.f;
+	
+protected:
+	UFUNCTION()
+	static TArray<FName> GetCollisionProfileNames();
+	
+	UPROPERTY(VisibleDefaultsOnly)
 	TObjectPtr<USceneComponent> StartPointComponent;
-	UPROPERTY(VisibleInstanceOnly)
+	
+	UPROPERTY(VisibleDefaultsOnly)
 	TObjectPtr<USceneComponent> EndPointComponent;
 
-	UPROPERTY(EditAnywhere, Category = "Collision Query", DisplayName="Method")
-	ECollisionQueryMethod QueryMethod = ECollisionQueryMethod::LineTrace;
-	
-	UPROPERTY(EditAnywhere, Category = "Collision Query", DisplayName="Type")
-	ECollisionQueryType QueryType = ECollisionQueryType::Single;
-	
-	UPROPERTY(EditAnywhere, Category = "Collision Query", DisplayName="By")
-	ECollisionQueryBy QueryBy = ECollisionQueryBy::Channel;
-	
 private:
-	void Query();
+	void Query(float DrawTime) const;
 	
+	void DoLineTraceSingle(float DrawTime) const;
+	void DoLineTraceMulti(float DrawTime) const;
+	void DoLineTraceTest(float DrawTime) const;
+	
+	void DoSweepSingle(float DrawTime) const;
+	void DoSweepMulti(float DrawTime) const;
+	void DoSweepTest(float DrawTime) const;
+	
+	void DoOverlapSingle(float DrawTime) const;
+	void DoOverlapMulti(float DrawTime) const;
+	void DoOverlapTest(float DrawTime) const;
+	
+	FCollisionQueryParams  GetCollisionQueryParams() const;
+	FCollisionResponseParams GetCollisionResponseParams() const;
+	FCollisionShape GetCollisionShape() const;
+	
+	static EQueryMobilityType ToQueryMobilityType(const ECollisionQueryTestMobility Mobility)
+	{
+		switch (Mobility)
+		{
+			using enum ECollisionQueryTestMobility;
+			case Any: 
+				return EQueryMobilityType::Any;
+			case Static: 
+				return EQueryMobilityType::Static;
+			case Dynamic: 
+				return EQueryMobilityType::Dynamic;
+			default: 
+				return EQueryMobilityType::Any;
+		}
+	}
+	
+	float TickTimer = 0;
 };
