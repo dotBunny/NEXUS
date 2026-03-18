5b07db846b42233a77180188e2c912a938934673
Matthew Davey <reapazor@gmail.com>
2026-03-18 14:09:47 -0400

    Missing constructor

--- Plugins/ProcGen/Source/NexusProcGen/Private/Generation/NProcGenOperationSharedContext.cpp
+++ Plugins/ProcGen/Source/NexusProcGen/Private/Generation/NProcGenOperationSharedContext.cpp
@@ -1,1 +1,8 @@
-﻿#include "Generation/NProcGenOperationSharedContext.h"
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+#include "Generation/NProcGenOperationSharedContext.h"
+
+FNProcGenOperationSharedContext::FNProcGenOperationSharedContext()
+{
+}
