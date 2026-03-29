5f29dde53e35778ecc80164b71461830b6cb5d92
Matthew Davey <reapazor@gmail.com>
2026-03-29 19:04:39 -0400

    Implement SetPositionZ

---
+++ Plugins/ProcGen/Source/NexusProcGen/Public/Elements/NSetPositionZElement.h
@@ -0,0 +1,50 @@
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+#pragma once
+
+#include "NColor.h"
+#include "PCGSettings.h"
+#include "NSetPositionZElement.generated.h"
+
+USTRUCT(BlueprintType)
+struct FNSetPositionZParams
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Setting)
+	float ZValue = 0.f;
+};
+
+UCLASS(BlueprintType, Blueprintable, Category="NEXUS")
+class UNSetPositionZSettings : public UPCGSettings
+{
+	GENERATED_BODY()
+
+public:
+	
+#if WITH_EDITOR
+	virtual FName GetDefaultNodeName() const override { return TEXT("NEXUS | Set Position Z"); }
+	virtual FText GetNodeTooltipText() const override { return INVTEXT("Squashes point data down to the designated Z-plane."); }
+	virtual FLinearColor GetNodeTitleColor() const override { return FNColor::GetElement; };
+	virtual EPCGSettingsType GetType() const override { return EPCGSettingsType::Spatial; }
+#endif
+	
+	virtual TArray<FPCGPinProperties> InputPinProperties() const override;
+	virtual TArray<FPCGPinProperties> OutputPinProperties() const override;
+	
+	virtual FPCGElementPtr CreateElement() const override;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta = (ShowOnlyInnerProperties, PCG_Overridable))
+	FNSetPositionZParams SetParams;
+};
+
+class FNSetPositionZElement : public IPCGElement
+{
+public:
+	virtual bool IsCacheable(const UPCGSettings* InSettings) const override { return true; }
+	virtual bool CanExecuteOnlyOnMainThread(FPCGContext* Context) const override { return true; }
+
+protected:
+	virtual bool ExecuteInternal(FPCGContext* Context) const override;
+};
