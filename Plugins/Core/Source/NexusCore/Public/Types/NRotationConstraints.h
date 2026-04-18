7ae868ae1d74dd45be7fd84f10da0162c36f3e10
Matthew Davey <reapazor@gmail.com>
2026-04-18 16:43:37 -0400

    Add logic for difference rotation constraint

--- Plugins/Core/Source/NexusCore/Public/Types/NRotationConstraints.h
+++ Plugins/Core/Source/NexusCore/Public/Types/NRotationConstraints.h
@@ -15,20 +15,20 @@ struct NEXUSCORE_API FNRotationConstraints
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Matching", DisplayName = "Enforce Matching?")
 	bool bEnforceMatchingRotation = true;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Matching", DisplayName = "Minimum Matching Rotation")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Matching", DisplayName = "Minimum Matching Rotation", meta=(EditCondition="bEnforceMatchingRotation", EditConditionHides))
 	FRotator MinimumMatchingRotation = FRotator(0.f, -180.f, 0.f);
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Matching", DisplayName = "Maximum Matching Rotation")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Matching", DisplayName = "Maximum Matching Rotation", meta=(EditCondition="bEnforceMatchingRotation", EditConditionHides))
 	FRotator MaximumMatchingRotation = FRotator(0.f, 180.f, 0.f);
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Difference", DisplayName = "Enforce Difference?")
 	bool bEnforceDifferenceRotation = false;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Difference", DisplayName = "Minimum Difference Rotation")
-	FQuat MinimumDifferenceRotation  = FQuat(0.f,0.f,0.f, 0.f);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Difference", DisplayName = "Minimum Difference Rotation", meta=(EditCondition="bEnforceDifferenceRotation", EditConditionHides))
+	FRotator MinimumDifferenceRotation  = FRotator(0.f, -180.f, 0.f);
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Difference", DisplayName = "Maximum Difference Rotation")
-	FQuat MaximumDifferenceRotation = FQuat(0.f,0.f,180.f, 0.f);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Difference", DisplayName = "Maximum Difference Rotation", meta=(EditCondition="bEnforceDifferenceRotation", EditConditionHides))
+	FRotator MaximumDifferenceRotation = FRotator(0.f, 180.f, 0.f);
 
 	bool CopyTo(FNRotationConstraints& Other) const
 	{
@@ -38,7 +38,9 @@ struct NEXUSCORE_API FNRotationConstraints
 
 	bool IsEqual(const FNRotationConstraints& Other) const
 	{
-		return  MinimumMatchingRotation == Other.MinimumMatchingRotation &&
+		return  bEnforceMatchingRotation == Other.bEnforceMatchingRotation &&
+				bEnforceDifferenceRotation == Other.bEnforceDifferenceRotation &&
+				MinimumMatchingRotation == Other.MinimumMatchingRotation &&
 				MaximumMatchingRotation == Other.MaximumMatchingRotation &&
 				MinimumDifferenceRotation == Other.MinimumDifferenceRotation &&
 				MaximumDifferenceRotation == Other.MaximumDifferenceRotation;
@@ -76,5 +78,39 @@ struct NEXUSCORE_API FNRotationConstraints
 			   NormalizedPitch <= MaximumMatchingRotation.Pitch + UE_KINDA_SMALL_NUMBER &&
 			   NormalizedYaw   >= MinimumMatchingRotation.Yaw   - UE_KINDA_SMALL_NUMBER &&
 			   NormalizedYaw   <= MaximumMatchingRotation.Yaw   + UE_KINDA_SMALL_NUMBER;
+	}
+	
+	bool IsDifferenceRotationAllowed(const FRotator& Rotation) const
+	{
+		if (!bEnforceDifferenceRotation)
+		{
+			return true;
+		}
+
+		const float Roll  = FRotator::NormalizeAxis(Rotation.Roll);
+		const float Pitch = FRotator::NormalizeAxis(Rotation.Pitch);
+		const float Yaw   = FRotator::NormalizeAxis(Rotation.Yaw);
+
+		return Roll  >= MinimumDifferenceRotation.Roll  - UE_KINDA_SMALL_NUMBER &&
+			   Roll  <= MaximumDifferenceRotation.Roll  + UE_KINDA_SMALL_NUMBER &&
+			   Pitch >= MinimumDifferenceRotation.Pitch - UE_KINDA_SMALL_NUMBER &&
+			   Pitch <= MaximumDifferenceRotation.Pitch + UE_KINDA_SMALL_NUMBER &&
+			   Yaw   >= MinimumDifferenceRotation.Yaw   - UE_KINDA_SMALL_NUMBER &&
+			   Yaw   <= MaximumDifferenceRotation.Yaw   + UE_KINDA_SMALL_NUMBER;
+	}
+	
+	bool IsDifferenceRotationAllowed(const float NormalizedRoll, const float NormalizedPitch, const float NormalizedYaw) const
+	{
+		if (!bEnforceDifferenceRotation)
+		{
+			return true;
+		}
+
+		return NormalizedRoll  >= MinimumDifferenceRotation.Roll  - UE_KINDA_SMALL_NUMBER &&
+			   NormalizedRoll  <= MaximumDifferenceRotation.Roll  + UE_KINDA_SMALL_NUMBER &&
+			   NormalizedPitch >= MinimumDifferenceRotation.Pitch - UE_KINDA_SMALL_NUMBER &&
+			   NormalizedPitch <= MaximumDifferenceRotation.Pitch + UE_KINDA_SMALL_NUMBER &&
+			   NormalizedYaw   >= MinimumDifferenceRotation.Yaw   - UE_KINDA_SMALL_NUMBER &&
+			   NormalizedYaw   <= MaximumDifferenceRotation.Yaw   + UE_KINDA_SMALL_NUMBER;
 	}
 };
