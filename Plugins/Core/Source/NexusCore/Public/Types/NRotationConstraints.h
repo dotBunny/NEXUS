cbe7119c2f88616c9253501194d8be5ff4298b04
Matthew Davey <reapazor@gmail.com>
2026-04-18 12:43:33 -0400

    Add more precise constraint mechanism

--- Plugins/Core/Source/NexusCore/Public/Types/NRotationConstraints.h
+++ Plugins/Core/Source/NexusCore/Public/Types/NRotationConstraints.h
@@ -5,19 +5,42 @@
 
 #include "NRotationConstraints.generated.h"
 
+
+
 USTRUCT(BlueprintType)
 struct NEXUSCORE_API FNRotationConstraints
 {
 	GENERATED_BODY()
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, DisplayName = "Allow Roll (X)")
+	// TODO: Replace with MinimumMatchingRotation.Roll & MinimumMatchingRotation.Roll based constraint
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Legacy", DisplayName = "Allow Roll (X)")
 	bool bRoll = false;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, DisplayName = "Allow Pitch (Y)")
+	// TODO: Replace with MinimumMatchingRotation.Roll & MinimumMatchingRotation.Roll based constraint
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Legacy", DisplayName = "Allow Pitch (Y)")
 	bool bPitch = false;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, DisplayName = "Allow Yaw (Z)")
+	
+	// TODO: Replace with MinimumMatchingRotation.Roll & MinimumMatchingRotation.Roll based constraint
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Legacy", DisplayName = "Allow Yaw (Z)")
 	bool bYaw = true;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Matching", DisplayName = "Enforce Matching?")
+	bool bEnforceMatchingRotation = true;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Matching", DisplayName = "Minimum Matching Rotation")
+	FQuat MinimumMatchingRotation = FQuat(0.f,0.f,0.f, 0.f);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Matching", DisplayName = "Maximum Matching Rotation")
+	FQuat MaximumMatchingRotation = FQuat(0.f,0.f,180.f, 0.f);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Difference", DisplayName = "Enforce Difference?")
+	bool bEnforceDifferenceRotation = false;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Difference", DisplayName = "Minimum Difference Rotation")
+	FQuat MinimumDifferenceRotation  = FQuat(0.f,0.f,0.f, 0.f);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Difference", DisplayName = "Maximum Difference Rotation")
+	FQuat MaximumDifferenceRotation = FQuat(0.f,0.f,180.f, 0.f);
 
 	bool CopyTo(FNRotationConstraints& Other) const
 	{
@@ -27,16 +50,11 @@ struct NEXUSCORE_API FNRotationConstraints
 
 	bool IsEqual(const FNRotationConstraints& Other) const
 	{
-		return  bRoll == Other.bRoll && bPitch == Other.bPitch && bYaw == Other.bYaw;
-	}
-	
-	bool IsAll() const
-	{
-		return bYaw && bPitch && bRoll;
-	}
-	
-	bool IsNone() const
-	{
-		return !bYaw && !bPitch && !bRoll;
+		return  bRoll == Other.bRoll && bPitch == Other.bPitch && bYaw == Other.bYaw &&
+				
+				MinimumMatchingRotation == Other.MinimumMatchingRotation &&
+				MaximumMatchingRotation == Other.MaximumMatchingRotation &&
+				MinimumDifferenceRotation == Other.MinimumDifferenceRotation &&
+				MaximumDifferenceRotation == Other.MaximumDifferenceRotation;
 	}
 };
