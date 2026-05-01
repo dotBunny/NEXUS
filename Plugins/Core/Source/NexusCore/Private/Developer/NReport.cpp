da82a73f8554c4fc5c1c10b5bf1ab5bcae5e1627
Matthew Davey <reapazor@gmail.com>
2026-05-01 15:24:11 -0400

    Rethink of the report

---
+++ Plugins/Core/Source/NexusCore/Private/Developer/NReport.cpp
@@ -0,0 +1,46 @@
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+#include "Developer/NReport.h"
+
+void FNReportContentBlock::AddContentBlock(const FNReportContentBlock& ChildContentBlock)
+{
+	int32 InsertIndex = Children.Num();
+	for (int32 i = 0; i < Children.Num(); i++)
+	{
+		if (Children[i].Priority > ChildContentBlock.Priority)
+		{
+			InsertIndex = i;
+			break;
+		}
+	}
+	
+	Children.Insert(ChildContentBlock, InsertIndex);
+}
+
+
+void FNReportContentBlock::RemoveContentBlock(const FNReportContentBlock& ChildContentBlock)
+{
+	if (Children.Contains(ChildContentBlock))
+	{
+		Children.Remove(ChildContentBlock);
+	}
+}
+
+FNReportContentBlock& FNReportContentBlock::CreateContentBlock()
+{
+	Children.Add(FNReportContentBlock());
+	return Children.Last();
+}
+
+FNReportContentBlock& FNReportContentBlock::CreateContentBlock(const FString& ContentHeading, const int OrderPriority)
+{
+	Children.Add(FNReportContentBlock());
+	
+	FNReportContentBlock& ContentBlock = Children.Last();
+	ContentBlock.Priority = OrderPriority;
+	ContentBlock.Heading = ContentHeading;
+	
+	
+	return ContentBlock;
+}
---
+++ Plugins/Core/Source/NexusCore/Public/Developer/NReport.h
@@ -0,0 +1,43 @@
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+#pragma once
+
+struct FNReport;
+/**
+ * Output format selector used by FNReport's emit methods to choose between human-readable
+ * plain text and Markdown.
+ */
+enum class ENTextOutputFormat : uint8
+{
+	/** Plain text formatted for the output log or a console window. */
+	PlainText,
+	/** Markdown-formatted text suitable for documentation, gists, or ticket attachments. */
+	Markdown
+};
+
+struct NEXUSCORE_API FNReportContentBlock
+{
+	FString Heading;
+	FString Content;
+	
+	void AddContentBlock(const FNReportContentBlock& ChildContentBlock);
+	void RemoveContentBlock(const FNReportContentBlock& ChildContentBlock);
+	FNReportContentBlock& CreateContentBlock();
+	FNReportContentBlock& CreateContentBlock(const FString& ContentHeading, int OrderPriority = 0);
+
+	bool operator==(const FNReportContentBlock& Other) const
+	{
+		// TODO: Actual ID?
+		return Heading == Other.Heading && Content == Other.Content && Priority == Other.Priority;
+	}
+
+private:
+	int Priority = 0;
+	TArray<FNReportContentBlock> Children;
+};
+
+struct NEXUSCORE_API FNReport
+{
+	FNReportContentBlock ContentRoot;
+};
