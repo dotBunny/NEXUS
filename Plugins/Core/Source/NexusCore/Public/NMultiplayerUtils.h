0cc54886b2cd0bf3caaa17412da974befb05cf52
Matthew Davey <reapazor@gmail.com>
2026-05-15 19:37:50 -0400

    Move out of If

--- Plugins/Core/Source/NexusCore/Public/NMultiplayerUtils.h
+++ Plugins/Core/Source/NexusCore/Public/NMultiplayerUtils.h
@@ -27,7 +27,8 @@ public:
 	 */
 	FORCEINLINE static void ServerTravel(const UObject* WorldContextObject, const FString& InURL, const bool bAbsolute = true, const bool bShouldSkipGameNotify = false)
 	{
-		if (UWorld* World = N_GET_WORLD_FROM_CONTEXT(WorldContextObject); IsValid(World))
+		UWorld* World = N_GET_WORLD_FROM_CONTEXT(WorldContextObject);
+		if (IsValid(World))
 		{
 			World->ServerTravel(InURL, bAbsolute, bShouldSkipGameNotify);
 		}
