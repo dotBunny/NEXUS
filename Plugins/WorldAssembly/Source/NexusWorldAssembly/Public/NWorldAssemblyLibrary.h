267d1d6ce9fbe7972c4e79eaa7e3b37d1dd1f2fa
Matthew Davey <reapazor@gmail.com>
2026-05-19 14:50:33 -0400

    Proper comment

--- Plugins/WorldAssembly/Source/NexusWorldAssembly/Public/NWorldAssemblyLibrary.h
+++ Plugins/WorldAssembly/Source/NexusWorldAssembly/Public/NWorldAssemblyLibrary.h
@@ -17,15 +17,7 @@ class UNWorldAssemblyLibrary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
-	/**
-	 * Compares two arrays of actors to determine if they contain the same actors.
-	 * @param A The first array of actors to compare.
-	 * @param B The second array of actors to compare.
-	 * @return True if both arrays contain the same set of actors (regardless of order), false otherwise.
-	 * @note This function checks if both arrays have the same length and contain the same actor references,
-	 *       regardless of their order in the arrays. Each actor in array A must only have exactly one matching
-	 *       actor in array B, and vice versa.
-	 */
+	/** @return A freshly generated human-friendly seed string suitable for use as FNAssemblyOperationSettings::Seed. */
 	UFUNCTION(BlueprintPure, Category = "NEXUS|WorldAssembly", DisplayName="Get New Friendly Seed")
-	static FString GetNewFriendlySeed() { return FNSeedGenerator::RandomFriendlySeed(); } 
+	static FString GetNewFriendlySeed() { return FNSeedGenerator::RandomFriendlySeed(); }
 };
