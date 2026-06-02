d9f02a73b740c4dfe7d42c024372c39937f35bed
Matthew Davey <reapazor@gmail.com>
2026-06-02 07:44:35 -0400

    stub out enums for usage with tag counter operations

---
+++ Plugins/Core/Source/NexusCore/Public/Types/NArithmeticOperation.h
@@ -0,0 +1,12 @@
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+#pragma once
+
+enum class ENGameplayTagCounterOperation : uint8
+{
+	Add,
+	Subtract,
+	Multiply,
+	Divide
+};
---
+++ Plugins/Core/Source/NexusCore/Public/Types/NComparisonResult.h
@@ -0,0 +1,13 @@
+﻿// Copyright dotBunny Inc. All Rights Reserved.
+// See the LICENSE file at the repository root for more information.
+
+#pragma once
+
+UENUM(BlueprintType)
+enum class ENComparisonResult : uint8
+{
+	Equal = 0 ,
+	GreaterThan = 1,
+	LessThan= 2,
+	NotEqual = 3
+};
