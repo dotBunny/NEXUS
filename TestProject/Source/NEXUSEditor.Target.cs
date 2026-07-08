643b5fda48d0245e8737619ccb8b137a551647cc
Matthew Davey <reapazor@gmail.com>
2026-07-08 10:10:31 -0400

    Target changes for 5.8

--- TestProject/Source/NEXUSEditor.Target.cs
+++ TestProject/Source/NEXUSEditor.Target.cs
@@ -8,8 +8,8 @@ public class NEXUSEditorTarget : TargetRules
 	public NEXUSEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("NEXUS");
 	}
 }
