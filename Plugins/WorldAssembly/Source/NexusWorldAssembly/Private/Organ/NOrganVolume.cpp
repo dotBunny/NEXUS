91654811b6529fa74c5ed6fdb7b97b5759e7cd3c
Matthew Davey <reapazor@gmail.com>
2026-08-23 16:30:57 -0400

    Fix for spatial loading

--- Plugins/WorldAssembly/Source/NexusWorldAssembly/Private/Organ/NOrganVolume.cpp
+++ Plugins/WorldAssembly/Source/NexusWorldAssembly/Private/Organ/NOrganVolume.cpp
@@ -17,4 +17,9 @@ ANOrganVolume::ANOrganVolume(const FObjectInitializer& ObjectInitializer)
 	BoneComponent->SetMobility(EComponentMobility::Static);
 	BoneComponent->SetRelativeLocation(FVector::ZeroVector);
 	BoneComponent->OrganComponent = OrganComponent;
+
+#if WITH_EDITORONLY_DATA
+	// IMPORTANT: If we  don't always load the organs, the bones will not be found for world assembly.
+	bIsSpatiallyLoaded = false;
+#endif
 }
