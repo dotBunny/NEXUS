25cfa11ad867457c9eee123af42137d15412ec72
Matthew Davey <reapazor@gmail.com>
2026-09-11 19:05:24 -0400

    Add Level Script option

--- Plugins/WorldAssembly/Source/NexusWorldAssembly/Public/Organ/NOrganComponent.h
+++ Plugins/WorldAssembly/Source/NexusWorldAssembly/Public/Organ/NOrganComponent.h
@@ -24,7 +24,8 @@ UENUM(BlueprintType)
 enum class ENOrganGenerationTrigger : uint8
 {
 	OnDemand = 0	UMETA(DisplayName="On Demand", ToolTip = "Generates only when requested (e.g. via Blueprint or Subsystem)."),
-	BeginPlay = 1	UMETA(DisplayName="Begin Play", ToolTip = "Generates when the component receives it's BeginPlay call.")
+	BeginPlay = 1	UMETA(DisplayName="Begin Play", ToolTip = "Generates when the component receives it's BeginPlay call."),
+	LevelScript = 2 UMETA(DisplayName="Level Script", ToolTip = "Specifically used for LevelScriptActor generation.")
 };
 
 /**
